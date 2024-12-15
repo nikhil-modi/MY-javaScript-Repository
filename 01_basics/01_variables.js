c082e81c1cc73cba02a13efb52ef7017c03998fe
nikhil-modi <149241905+nikhil-modi@users.noreply.github.com>
2024-12-15 12:25:44 +0000

    variable_file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 15543
+let accountEmail = "nikhil@gmail.com"
+var accountPassword = "45123"
+accountCity = "Delhi"
+
+accountEmail = "nik@d.com"
+accountPassword = "3345"
+accountCity = "goa"
+
+// accountId = 2
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
