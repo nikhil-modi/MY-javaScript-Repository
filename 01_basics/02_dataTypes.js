d6ff1345f324e393b4c3024c05908a7937a03e79
nikhil-modi <149241905+nikhil-modi@users.noreply.github.com>
2024-12-17 16:45:13 +0000

    conversion of DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all JS code as newer version
+
+// different data types
+// number => 2 to power 53
+// string 
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// here NaN means "Not a Number" this is given when we type something like 33abc and try to convert this string into number
+//Nan is also a data type
+
+// "33abc" => NaN
+// true => 1 and false = 0
+
+// coversion of boolean 
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nikhil" => true
+
+
+let sumNumber = 33
+
+let StringNumber = String(sumNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
+
+
+
