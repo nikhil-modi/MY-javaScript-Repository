d7ca138b1c1d596a77f0e4e68a9e7b78dcd1918a
nikhil-modi <149241905+nikhil-modi@users.noreply.github.com>
2024-12-25 13:58:13 +0000

    chapter4

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log("2" > 1);
+
+// the equality check === and comparision "> < >=" works differently 
+// comparision convert null to a number treating it as 0
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// that's why null>=0 is true and null>0 is false
+
+// === this is called as strict check which also checks the datatypes
+
+
+
+
+
