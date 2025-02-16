cb7fc6987c3b521c9b411084e82be63d955b4354
nikhil-modi <149241905+nikhil-modi@users.noreply.github.com>
2025-02-16 06:14:23 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+let index = 0
+
+while (index <=10) {
+    console.log(`value of the index is ${index}`);
+    index = index+2
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+
+while(arr < myArray.length){
+    console.log(`value of the heroes is ${myArray[arr]}`);
+    arr = arr+1
+    
+}
+
+do {
+    
+} while (condition);
+
+
+
+
+
